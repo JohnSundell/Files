a311c76ac2217e0382342ed52d0212dcc37204e2
John Sundell <john@sundell.co>
2017-11-05 20:34:57 +0100

    Use Swift 4 version of the Package Description API

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:4.0
+
+/**
+ *  Files
+ *  Copyright (c) John Sundell 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
 import PackageDescription
 
 let package = Package(
-    name: "Files"
+    name: "Files",
+    products: [
+        .library(name: "Files", targets: ["Files"])
+    ],
+    targets: [
+        .target(
+            name: "Files",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "FilesTests",
+            dependencies: ["Files"]
+        )
+    ]
 )
